e4be401c1d5f9b8281692095b32a71660b5ac715
gracepurple <92028443+gracepurple@users.noreply.github.com>
2022-10-08 13:22:22 +0530

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,38 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+const fs = require('fs');
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/nAhiCHKvZkhkp4A7PkkCIBON0-BXW26d`,
+      //accounts: [process.env.privateKey]
+    },
+    matic: {
+      url: "https://polygon-mainnet.g.alchemy.com/v2/vD4ASgxHMy0stFwKZiWTPfFseAFRp8cm",
+      accounts: [bfbaeaf25222ff22cf2a6af9676458ef5ef69c924eeaddad52a95e615dd776d0]
+    },
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
